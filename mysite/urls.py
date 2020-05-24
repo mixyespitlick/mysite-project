44f5518e8c9547b80c395ccd41be03d46cc5e6c6
mixyespitlick <mascarinasarniel@gmail.com>
2020-05-24 17:17:43 +0800

    Adding mysite blog  project

---
+++ mysite/urls.py
@@ -0,0 +1,46 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from personal.views import (home_screen_view,)
+from account.views import (
+    registration_view,
+    logout_view,
+    logout_view,
+    login_view,
+    account_view,
+    must_authenticate_view
+)
+
+urlpatterns = [
+    path('', home_screen_view, name="home"),
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', 'blog')),
+    path('account/', account_view, name="account"),
+    path('login/', login_view, name="login"),
+    path('logout/', logout_view, name="logout"),
+    path('must_authenticate/', must_authenticate_view, name="must_authenticate"),
+    path('register/', registration_view, name="register"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
---
+++ personal/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# tuple
+# PRIORITY = [
+#     ('H', 'High'),
+#     ('M', 'Medium'),
+#     ('L', 'Low'),
+# ]
+
+
+# class Question(models.Model):
+#     title = models.CharField(max_length=50)
+#     question = models.CharField(max_length=200)
+#     priority = models.CharField(max_length=1, choices=PRIORITY)
+
+#     def __str__(self):
+#         return self.title
+
+#     class Meta:
+#         verbose_name = 'The Question'
+#         verbose_name_plural = 'Peoples Questions'
